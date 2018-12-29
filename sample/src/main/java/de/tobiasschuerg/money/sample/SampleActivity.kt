d4bb5a197037d080caa47bc28a5e4570954868e3
Tobias Schürg <tobiasschuerg@gmail.com>
2018-12-29 10:39:39 +0100

    Migrated to AndroidX and updated Kotlin to 1.3.11.

--- sample/src/main/java/de/tobiasschuerg/money/sample/SampleActivity.kt
+++ sample/src/main/java/de/tobiasschuerg/money/sample/SampleActivity.kt
@@ -2,7 +2,7 @@ package de.tobiasschuerg.money.sample
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import de.tobiasschuerg.money.Currencies
 import de.tobiasschuerg.money.Currency
